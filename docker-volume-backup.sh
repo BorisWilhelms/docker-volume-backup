27a4d9fbb1a39992d54a6dd0864b5abab2906b8e
Boris Wilhelms <mail@wille-zone.de>
2022-02-02 23:51:47 +0100

    Adds implementation

--- docker-volume-backup.sh
+++ docker-volume-backup.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
+DEST_PATH=${1-.}
 
-# check if docker is installed
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Error: docker is not installed.' >&2
   exit 1
 fi
 
-# check if jq is installed
 if ! [ -x "$(command -v jq)" ]; then
   echo 'Error: jq is not installed.' >&2
   exit 1
 fi
 
-VOLUMES=$(docker volume inspect $(docker volume ls --format "{{ .Name }}" --filter driver=local) \
-    | jq -r '.[].Name') 
+if ! [ -x "$(command -v openssl)" ]; then
+  echo 'Error: openssl is not installed.' >&2
+  exit 1
+fi
 
-tar -czf volumes.tar.gz $VOLUMES
+restart_containers() {
+  docker start $RUNNING_CONTAINERS >> /dev/null  
+}
+
+trap restart_containers EXIT
+
+RUNNING_CONTAINERS=$(docker ps -q)
+docker stop $RUNNING_CONTAINERS >> /dev/null
+ 
+VOLUMES=$(docker volume inspect $(docker volume ls --format "{{ .Name }}" --filter driver=local) \
+    | jq -r '.[].Mountpoint') 
+
+# tar all volumes and encrypt them to a file with the current date in the name
+DEST_FILE="$DEST_PATH/docker-volume-backup-$(date +%Y-%m-%d-%H-%M-%S).tar.gz"
+tar -czPf "$DEST_FILE" $VOLUMES
+
+echo "$DEST_FILE"
